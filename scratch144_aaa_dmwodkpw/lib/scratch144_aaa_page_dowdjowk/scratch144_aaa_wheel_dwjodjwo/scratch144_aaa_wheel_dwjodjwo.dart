e8a0efaa228e77b16f9c159d509b618bce315c58
chengmengyi <519097965@qq.com>
2026-03-06 15:07:29 +0800

    1

--- scratch144_aaa_dmwodkpw/lib/scratch144_aaa_page_dowdjowk/scratch144_aaa_wheel_dwjodjwo/scratch144_aaa_wheel_dwjodjwo.dart
+++ scratch144_aaa_dmwodkpw/lib/scratch144_aaa_page_dowdjowk/scratch144_aaa_wheel_dwjodjwo/scratch144_aaa_wheel_dwjodjwo.dart
@@ -4,6 +4,7 @@ import 'package:scratch144_base_dwhidjwo/scratch144_page_yyclzs/scratch144_widge
 import 'package:scratch144_base_dwhidjwo/scratch144_page_yyclzs/scratch144_widget_type_vsmaou.dart';
 import 'package:scratch144_base_dwhidjwo/scratch144_utils_zhnggc/scratch144_colors_tvffoz.dart';
 import 'package:scratch144_base_dwhidjwo/scratch144_utils_zhnggc/scratch144_utils_zhnggc.dart';
+import 'package:scratch144_base_dwhidjwo/scratch144_widget_inhgkd/scratch144_local_images_widget_gohzrl.dart';
 import 'package:scratch144_base_dwhidjwo/scratch144_widget_inhgkd/scratch144_text_widget_pcbuin.dart';
 
 class Scratch144AaaWheelDwjodjwo extends Scratch144WidgetInhgkd<Scratch144AaaWheelControllerDwjodjwo>{
@@ -14,7 +15,44 @@ class Scratch144AaaWheelDwjodjwo extends Scratch144WidgetInhgkd<Scratch144AaaWhe
   Scratch144WidgetTypezeuaqu scratch144_init_type_hqrwcn() => Scratch144WidgetTypezeuaqu.child;
 
   @override
-  Widget scratch144_init_widget_xnblzi() => Center(
-    child: Scratch144TextWidgetPcbuin(scratch144Textannbiq: "wheel", scratch144Sizefbwmhh: 20.sp, scratch144TextColorrzkydb: Scratch144nwekyj.color000000),
+  Widget scratch144_init_widget_xnblzi() => Stack(
+    alignment: Alignment.topCenter,
+    children: [
+      Scratch144LocalImagesWidgetGohzrl(scratch144Nametrrwib: "wheel1", scratch144Widthcpygxw: double.infinity, scratch144Heightvnnnnq: double.infinity),
+      Container(
+        padding: EdgeInsets.only(top: 110.h, bottom: 100.h),
+        child: Column(
+          children: [
+            SizedBox(height: 40.h,),
+            Scratch144LocalImagesWidgetGohzrl(scratch144Nametrrwib: "wheel2", scratch144Widthcpygxw: null, scratch144Heightvnnnnq: 32.h,boxFit: BoxFit.fitHeight,),
+            SizedBox(height: 30.h,),
+            _wheelWidget(),
+          ],
+        ),
+      ),
+    ],
+  );
+
+  _wheelWidget()=>Container(
+    margin: EdgeInsets.only(left: 10.w,right: 10.w),
+    child: Stack(
+      alignment: Alignment.bottomCenter,
+      children: [
+        Scratch144LocalImagesWidgetGohzrl(scratch144Nametrrwib: "wheel4", scratch144Widthcpygxw: 272.w, scratch144Heightvnnnnq: 76.h),
+        Container(
+          margin: EdgeInsets.only(bottom: 56.h),
+          child: AspectRatio(
+            aspectRatio: 1,
+            child: Stack(
+              alignment: Alignment.center,
+              children: [
+                Scratch144LocalImagesWidgetGohzrl(scratch144Nametrrwib: "wheel3", scratch144Widthcpygxw: double.infinity, scratch144Heightvnnnnq: double.infinity),
+                Scratch144LocalImagesWidgetGohzrl(scratch144Nametrrwib: "wheel5", scratch144Widthcpygxw: 104.w, scratch144Heightvnnnnq: 126.h),
+              ],
+            ),
+          ),
+        )
+      ],
+    ),
   );
 }
