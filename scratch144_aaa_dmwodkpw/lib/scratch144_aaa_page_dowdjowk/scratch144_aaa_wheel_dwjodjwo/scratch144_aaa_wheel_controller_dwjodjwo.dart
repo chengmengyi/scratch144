f9023b0b55c24dbb165ba9a6e259db9d5ab0a729
chengmengyi <519097965@qq.com>
2026-03-06 15:31:17 +0800

    1

--- scratch144_aaa_dmwodkpw/lib/scratch144_aaa_page_dowdjowk/scratch144_aaa_wheel_dwjodjwo/scratch144_aaa_wheel_controller_dwjodjwo.dart
+++ scratch144_aaa_dmwodkpw/lib/scratch144_aaa_page_dowdjowk/scratch144_aaa_wheel_dwjodjwo/scratch144_aaa_wheel_controller_dwjodjwo.dart
@@ -1,8 +1,159 @@
+import 'dart:async';
+import 'dart:math';
+
+import 'package:flutter/animation.dart';
+import 'package:scratch144_aaa_dmwodkpw/scratch144_aaa_dialog_dwijijfie/scratch144_aaa_win_dialog_fjeifjoej/scratch144_aaa_win_dialog_fjeifjoej.dart';
 import 'package:scratch144_base_dwhidjwo/scratch144_page_yyclzs/scratch144_con_sspacr.dart';
+import 'package:scratch144_base_dwhidjwo/scratch144_routers_nxtyun/scratch144_router_utils_dnwkdjow.dart';
 
-class Scratch144AaaWheelControllerDwjodjwo extends Scratch144ConSspacr{
+class Scratch144AaaWheelControllerDwjodjwo extends Scratch144ConSspacr {
+  final List<int> scratch144_wheel_rewards_hqzvpt = const <int>[
+    10,
+    10,
+    80,
+    500,
+    10,
+    20,
+    50,
+    100,
+  ];
+  final Random _scratch144_random_mpqvtx = Random();
+  Timer? _scratch144_spin_timer_pqvmtx;
 
-  clickStart(){
+  double scratch144_wheel_angle_qvptxm = 0.0;
+  bool scratch144_spinning_hqmvpt = false;
+  bool scratch144_show_finger_guide_qmvpt = false;
+  int? scratch144_last_reward_qmvptx;
+  int? scratch144_last_index_qmvptx;
 
+  String get scratch144_reward_text_vqpmxt {
+    if (scratch144_last_reward_qmvptx == null) {
+      return "Tap center to spin";
+    }
+    return "Reward: $scratch144_last_reward_qmvptx";
+  }
+
+  Future<int?> clickStart() async {
+    if (scratch144_spinning_hqmvpt) {
+      return null;
+    }
+    scratch144_show_finger_guide_qmvpt = false;
+    scratch144_spinning_hqmvpt = true;
+    update([
+      "scratch144_wheel_spin_state_qmvpt",
+      "scratch144_wheel_finger_guide_qmvpt",
+    ]);
+
+    final int scratch144_target_index_vqpmxt = _scratch144_random_mpqvtx
+        .nextInt(scratch144_wheel_rewards_hqzvpt.length);
+    final double scratch144_target_angle_uqmvpt =
+        _scratch144_calc_target_angle_qmvptx(scratch144_target_index_vqpmxt);
+    final double scratch144_total_delta_mqvptx =
+        _scratch144_calc_total_delta_wqmvpt(scratch144_target_angle_uqmvpt);
+
+    await _scratch144_start_spin_anim_qmvptx(scratch144_total_delta_mqvptx);
+
+    scratch144_last_index_qmvptx = scratch144_target_index_vqpmxt;
+    scratch144_last_reward_qmvptx =
+        scratch144_wheel_rewards_hqzvpt[scratch144_target_index_vqpmxt];
+
+    update([
+      "scratch144_wheel_spin_state_qmvpt",
+      "scratch144_wheel_result_qmpvtx",
+    ]);
+    await Future.delayed(Duration(milliseconds: 2000));
+    scratch144_spinning_hqmvpt = false;
+    Scratch144RouterUtilsDnwkdjow.instance.showDialogwouwojdw(
+      child: Scratch144AaaWinDialogFjeifjoej(
+        reward: scratch144_last_reward_qmvptx!,
+        callback: () {
+          scratch144_show_finger_guide_qmvpt = true;
+          update(["scratch144_wheel_finger_guide_qmvpt"]);
+        },
+      ),
+    );
+    return scratch144_last_reward_qmvptx;
+  }
+
+  Future<void> _scratch144_start_spin_anim_qmvptx(
+    double scratch144_total_delta_mqvptx,
+  ) {
+    const int scratch144_total_ms_qmvptx = 3000;
+    const int scratch144_tick_ms_qmvptx = 16;
+    final Completer<void> scratch144_completer_qmvptx = Completer<void>();
+    final double scratch144_start_angle_qmvptx = scratch144_wheel_angle_qvptxm;
+    int scratch144_elapsed_ms_qmvptx = 0;
+
+    _scratch144_spin_timer_pqvmtx?.cancel();
+    _scratch144_spin_timer_pqvmtx = Timer.periodic(
+      const Duration(milliseconds: scratch144_tick_ms_qmvptx),
+      (Timer scratch144_timer_qmvptx) {
+        scratch144_elapsed_ms_qmvptx += scratch144_tick_ms_qmvptx;
+        final bool scratch144_done_qmvptx =
+            scratch144_elapsed_ms_qmvptx >= scratch144_total_ms_qmvptx;
+        final double scratch144_progress_qmvptx = scratch144_done_qmvptx
+            ? 1.0
+            : scratch144_elapsed_ms_qmvptx / scratch144_total_ms_qmvptx;
+        final double scratch144_curve_progress_qmvptx = Curves.easeOutCubic
+            .transform(scratch144_progress_qmvptx);
+        scratch144_wheel_angle_qvptxm =
+            scratch144_start_angle_qmvptx +
+            scratch144_total_delta_mqvptx * scratch144_curve_progress_qmvptx;
+        update(["scratch144_wheel_rotate_qmpvtx"]);
+        if (!scratch144_done_qmvptx) {
+          return;
+        }
+        scratch144_timer_qmvptx.cancel();
+        _scratch144_spin_timer_pqvmtx = null;
+        if (!scratch144_completer_qmvptx.isCompleted) {
+          scratch144_completer_qmvptx.complete();
+        }
+      },
+    );
+    return scratch144_completer_qmvptx.future;
+  }
+
+  double _scratch144_calc_target_angle_qmvptx(
+    int scratch144_target_index_vqpmxt,
+  ) {
+    final double scratch144_segment_angle_qmvptx =
+        (2 * pi) / scratch144_wheel_rewards_hqzvpt.length;
+    final double scratch144_center_angle_qmvptx =
+        -(scratch144_target_index_vqpmxt * scratch144_segment_angle_qmvptx);
+    final double scratch144_random_offset_qmvptx =
+        (_scratch144_random_mpqvtx.nextDouble() - 0.5) *
+        scratch144_segment_angle_qmvptx *
+        0.72;
+    return scratch144_center_angle_qmvptx + scratch144_random_offset_qmvptx;
+  }
+
+  double _scratch144_calc_total_delta_wqmvpt(
+    double scratch144_target_angle_uqmvpt,
+  ) {
+    final double scratch144_start_normal_qmvptx =
+        _scratch144_normalize_angle_qmvptx(scratch144_wheel_angle_qvptxm);
+    final double scratch144_target_normal_qmvptx =
+        _scratch144_normalize_angle_qmvptx(scratch144_target_angle_uqmvpt);
+    double scratch144_delta_qmvptx =
+        scratch144_target_normal_qmvptx - scratch144_start_normal_qmvptx;
+    if (scratch144_delta_qmvptx < 0) {
+      scratch144_delta_qmvptx += 2 * pi;
+    }
+    return scratch144_delta_qmvptx + (2 * pi * 6);
+  }
+
+  double _scratch144_normalize_angle_qmvptx(double scratch144_angle_qmvptx) {
+    double scratch144_normal_qmvptx = scratch144_angle_qmvptx % (2 * pi);
+    if (scratch144_normal_qmvptx < 0) {
+      scratch144_normal_qmvptx += 2 * pi;
+    }
+    return scratch144_normal_qmvptx;
+  }
+
+  @override
+  void onClose() {
+    _scratch144_spin_timer_pqvmtx?.cancel();
+    _scratch144_spin_timer_pqvmtx = null;
+    super.onClose();
   }
 }
--- scratch144_aaa_dmwodkpw/lib/scratch144_aaa_page_dowdjowk/scratch144_aaa_wheel_dwjodjwo/scratch144_aaa_wheel_dwjodjwo.dart
+++ scratch144_aaa_dmwodkpw/lib/scratch144_aaa_page_dowdjowk/scratch144_aaa_wheel_dwjodjwo/scratch144_aaa_wheel_dwjodjwo.dart
@@ -2,31 +2,42 @@ import 'package:flutter/material.dart';
 import 'package:scratch144_aaa_dmwodkpw/scratch144_aaa_page_dowdjowk/scratch144_aaa_wheel_dwjodjwo/scratch144_aaa_wheel_controller_dwjodjwo.dart';
 import 'package:scratch144_base_dwhidjwo/scratch144_page_yyclzs/scratch144_widget_inhgkd.dart';
 import 'package:scratch144_base_dwhidjwo/scratch144_page_yyclzs/scratch144_widget_type_vsmaou.dart';
-import 'package:scratch144_base_dwhidjwo/scratch144_utils_zhnggc/scratch144_colors_tvffoz.dart';
 import 'package:scratch144_base_dwhidjwo/scratch144_utils_zhnggc/scratch144_utils_zhnggc.dart';
 import 'package:scratch144_base_dwhidjwo/scratch144_widget_inhgkd/scratch144_click_widget_mplwfm.dart';
+import 'package:scratch144_base_dwhidjwo/scratch144_widget_inhgkd/scratch144_finger_widget_upnejl.dart';
 import 'package:scratch144_base_dwhidjwo/scratch144_widget_inhgkd/scratch144_local_images_widget_gohzrl.dart';
-import 'package:scratch144_base_dwhidjwo/scratch144_widget_inhgkd/scratch144_text_widget_pcbuin.dart';
 
-class Scratch144AaaWheelDwjodjwo extends Scratch144WidgetInhgkd<Scratch144AaaWheelControllerDwjodjwo>{
+class Scratch144AaaWheelDwjodjwo
+    extends Scratch144WidgetInhgkd<Scratch144AaaWheelControllerDwjodjwo> {
   @override
-  Scratch144AaaWheelControllerDwjodjwo scratch144_init_con_djqgmr() => Scratch144AaaWheelControllerDwjodjwo();
+  Scratch144AaaWheelControllerDwjodjwo scratch144_init_con_djqgmr() =>
+      Scratch144AaaWheelControllerDwjodjwo();
 
   @override
-  Scratch144WidgetTypezeuaqu scratch144_init_type_hqrwcn() => Scratch144WidgetTypezeuaqu.child;
+  Scratch144WidgetTypezeuaqu scratch144_init_type_hqrwcn() =>
+      Scratch144WidgetTypezeuaqu.child;
 
   @override
   Widget scratch144_init_widget_xnblzi() => Stack(
     alignment: Alignment.topCenter,
     children: [
-      Scratch144LocalImagesWidgetGohzrl(scratch144Nametrrwib: "wheel1", scratch144Widthcpygxw: double.infinity, scratch144Heightvnnnnq: double.infinity),
+      Scratch144LocalImagesWidgetGohzrl(
+        scratch144Nametrrwib: "wheel1",
+        scratch144Widthcpygxw: double.infinity,
+        scratch144Heightvnnnnq: double.infinity,
+      ),
       Container(
         padding: EdgeInsets.only(top: 110.h, bottom: 100.h),
         child: Column(
           children: [
-            SizedBox(height: 40.h,),
-            Scratch144LocalImagesWidgetGohzrl(scratch144Nametrrwib: "wheel2", scratch144Widthcpygxw: null, scratch144Heightvnnnnq: 32.h,boxFit: BoxFit.fitHeight,),
-            SizedBox(height: 30.h,),
+            SizedBox(height: 40.h),
+            Scratch144LocalImagesWidgetGohzrl(
+              scratch144Nametrrwib: "wheel2",
+              scratch144Widthcpygxw: null,
+              scratch144Heightvnnnnq: 32.h,
+              boxFit: BoxFit.fitHeight,
+            ),
+            SizedBox(height: 30.h),
             _wheelWidget(),
           ],
         ),
@@ -34,30 +45,91 @@ class Scratch144AaaWheelDwjodjwo extends Scratch144WidgetInhgkd<Scratch144AaaWhe
     ],
   );
 
-  _wheelWidget()=>Container(
-    margin: EdgeInsets.only(left: 10.w,right: 10.w),
-    child: Stack(
-      alignment: Alignment.bottomCenter,
+  _wheelWidget() => Container(
+    margin: EdgeInsets.only(left: 10.w, right: 10.w),
+    child: Column(
       children: [
-        Scratch144LocalImagesWidgetGohzrl(scratch144Nametrrwib: "wheel4", scratch144Widthcpygxw: 272.w, scratch144Heightvnnnnq: 76.h),
-        Container(
-          margin: EdgeInsets.only(bottom: 56.h),
-          child: AspectRatio(
-            aspectRatio: 1,
-            child: Stack(
-              alignment: Alignment.center,
-              children: [
-                Scratch144LocalImagesWidgetGohzrl(scratch144Nametrrwib: "wheel3", scratch144Widthcpygxw: double.infinity, scratch144Heightvnnnnq: double.infinity),
-                Scratch144ClickWidgetMplwfm(
-                  onTap: (){
-                    scratch144Controllerwjbgnj.clickStart();
-                  },
-                  child: Scratch144LocalImagesWidgetGohzrl(scratch144Nametrrwib: "wheel5", scratch144Widthcpygxw: 104.w, scratch144Heightvnnnnq: 126.h),
-                ),
-              ],
+        Stack(
+          alignment: Alignment.bottomCenter,
+          children: [
+            Scratch144LocalImagesWidgetGohzrl(
+              scratch144Nametrrwib: "wheel4",
+              scratch144Widthcpygxw: 272.w,
+              scratch144Heightvnnnnq: 76.h,
             ),
-          ),
-        )
+            Container(
+              margin: EdgeInsets.only(bottom: 56.h),
+              child: AspectRatio(
+                aspectRatio: 1,
+                child: Stack(
+                  alignment: Alignment.center,
+                  children: [
+                    GetBuilder<Scratch144AaaWheelControllerDwjodjwo>(
+                      id: "scratch144_wheel_rotate_qmpvtx",
+                      builder: (_) => Transform.rotate(
+                        angle: scratch144Controllerwjbgnj
+                            .scratch144_wheel_angle_qvptxm,
+                        child: Scratch144LocalImagesWidgetGohzrl(
+                          scratch144Nametrrwib: "wheel3",
+                          scratch144Widthcpygxw: double.infinity,
+                          scratch144Heightvnnnnq: double.infinity,
+                        ),
+                      ),
+                    ),
+                    GetBuilder<Scratch144AaaWheelControllerDwjodjwo>(
+                      id: "scratch144_wheel_spin_state_qmvpt",
+                      builder: (_) => Stack(
+                        alignment: Alignment.center,
+                        children: [
+                          Scratch144ClickWidgetMplwfm(
+                            onTap:
+                                scratch144Controllerwjbgnj
+                                    .scratch144_spinning_hqmvpt
+                                ? null
+                                : () {
+                                    scratch144Controllerwjbgnj.clickStart();
+                                  },
+                            child: Opacity(
+                              opacity:
+                                  scratch144Controllerwjbgnj
+                                      .scratch144_spinning_hqmvpt
+                                  ? 0.85
+                                  : 1,
+                              child: Scratch144LocalImagesWidgetGohzrl(
+                                scratch144Nametrrwib: "wheel5",
+                                scratch144Widthcpygxw: 104.w,
+                                scratch144Heightvnnnnq: 126.h,
+                              ),
+                            ),
+                          ),
+                          GetBuilder<Scratch144AaaWheelControllerDwjodjwo>(
+                            id: "scratch144_wheel_finger_guide_qmvpt",
+                            builder: (_) => Visibility(
+                              visible:
+                                  scratch144Controllerwjbgnj
+                                      .scratch144_show_finger_guide_qmvpt &&
+                                  !scratch144Controllerwjbgnj
+                                      .scratch144_spinning_hqmvpt,
+                              child: Container(
+                                margin: EdgeInsets.only(left: 40.w,top: 90.h),
+                                child: IgnorePointer(
+                                  child: Scratch144FingerWidgetUpnejl(
+                                    scratch144Widthcpygxw: 63.w,
+                                    scratch144Heightvnnnnq: 63.w,
+                                  ),
+                                ),
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
       ],
     ),
   );
